820e648cd884df8ada39b1e4db318acf4fd35712
Tom <tomtarpeydev@gmail.com>
2020-02-17 10:31:51 +0000

    factorial

--- bigo.py
+++ bigo.py
@@ -24,5 +24,41 @@ def quadratic_time(items):
         for item2 in items: # O(n) * O(1)
             print(item, ' ', item2) # O(1)
 
+# chalenge. What is the complexity of this algorithm? = O(n)
 
+def complex_algo(items):
+
+    for _ in range(5):
+        print ("Python is awesome")
+
+    for item in items:
+        print(item)
+
+    for item in items:
+        print(item)
+
+    print("Big O")
+    print("Big O")
+    print("Big O")
+
+complex_algo([4, 5, 6, 8])
+
+# itterative solution
+# factorial
+# O(n)
+def fact(n):
+    product = 1 # O(1)
+    # loop over a range 
+    for i in range(n): # O(n) * O(1)
+        # multiply the range by the product and assign the result to the product
+        product = product * (i + 1) # O(1)
+    # return the product
+    return product # O(1)
+
+def fact_r(n):
+    # base case
+    if n == 0: # O(1)
+        return 1 # O(1)
+    # recursive call
+    return n * fact_r(n - 1) # O(n)
 
