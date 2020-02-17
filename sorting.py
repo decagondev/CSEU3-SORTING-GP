dc27fe4f5a8e2884aa5c65b26c3ecfc182fa7331
Tom <tomtarpeydev@gmail.com>
2020-02-17 12:35:48 +0000

    simple insertion sort

--- sorting.py
+++ sorting.py
@@ -36,14 +36,64 @@ def in_sort(books):
 
     return books
 
-for b in books:
-    print(b)
+# for b in books:
+#     print(b)
 
-print('---------------------')
+# print('---------------------')
 
-in_sort(books)
+# in_sort(books)
 
 
-for b in books:
-    print(b)
+# for b in books:
+#     print(b)
 
+"""
+- **Insertion Sort** is an _in-place_ algorithm, meaning that it 
+  does not require any additional memory to perform the sort operation.
+
+- It works by conceptually dividing the array into _sorted_ and _unsorted_ pieces.
+
+    1. Consider element at index 0 to be our  _sorted_ piece. The rest of the array is our _unsorted_ piece.
+
+    2. Save the 1st element in the _unsorted_ piece in a temp variable.
+
+    3. Shift elements in the _sorted_ piece over to the right until we find where the element 
+       from step 2 should go.
+
+    4. Insert the element from step 2 into its correct index within the _sorted_ piece.
+
+    5. Repeat steps 2-4 until all elements have been processed.
+"""
+
+def in_sort2(lst):
+    # loop over n - 1 elements
+    for i in range(1, len(lst)):
+        # save initial element to temp variable
+        temp = lst[i]
+        # set inner loop index to current index
+        j = i
+        # inner loop
+        while j > 0 and temp < lst[j - 1]:
+            # shift left until correct position found
+            lst[j] = lst[j - 1]
+            # decrement inner index
+            j -= 1
+        # insert temp at correct position
+        lst[j] = temp
+    # return our list
+    return lst
+
+my_nums = [23, 34, 60, 1, 4, 5, 2]
+my_names = ['Dave', 'Steve', 'Bob']
+
+print(my_nums)
+
+in_sort2(my_nums)
+
+print(my_nums)
+
+print(my_names)
+
+in_sort2(my_names)
+
+print(my_names)
